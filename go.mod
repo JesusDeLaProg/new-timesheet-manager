ef52d890c0c1b7b991084999981030b1457228da
Maxime Charland <charland.maxime@gmail.com>
2025-04-20 23:06:02 -0400

    Finish Bazel setup and complete proto definitions

--- go.mod
+++ go.mod
@@ -1,16 +1,16 @@
-module github.com/JesusDeLaProg/timesheet-manager
+module github.com/JesusDeLaProg/new-timesheet-manager
 
-go 1.24.0
+go 1.24.2
 
 require (
-	google.golang.org/genproto v0.0.0-20250303144028-a0af3efb3deb
-	google.golang.org/grpc v1.71.0
-	google.golang.org/protobuf v1.36.5
+	google.golang.org/genproto v0.0.0-20250414145226-207652e42e2e
+	google.golang.org/grpc v1.71.1
+	google.golang.org/protobuf v1.36.6
 )
 
 require (
-	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250227231956-55c901821b1e // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250414145226-207652e42e2e // indirect
 )
--- server/main.go
+++ server/main.go
@@ -1,38 +1,30 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 
-	_ "github.com/JesusDeLaProg/timesheet-manager/proto"
+	pb "github.com/JesusDeLaProg/new-timesheet-manager/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 var port = flag.Int("port", 8080, "The server port")
 
-// type greetingServer struct {
-// 	pb.UnimplementedGreetingServer
-// }
-//
-// type emptyNameError struct{}
-//
-// func (emptyNameError) Error() string {
-// 	return "empty name"
-// }
-//
-// func (s *greetingServer) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
-// 	if in.GetName() == "" {
-// 		return nil, emptyNameError{}
-// 	}
-// 	message := fmt.Sprintf("Hello, %s!", in.GetName())
-// 	out := &pb.GreetResponse{
-// 		Message: &message,
-// 	}
-// 	return out, nil
-// }
+type ActivityServer struct{}
+
+func (s *ActivityServer) Fetch(ctx context.Context, r *pb.FetchActivitiesRequest) (*pb.FetchActivitiesResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "Fetch is unimplemented")
+}
+
+func (s *ActivityServer) Save(ctx context.Context, r *pb.SaveActivitiesRequest) (*pb.SaveActivitiesResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "Save is unimplemented")
+}
 
 func main() {
 	flag.Parse()
@@ -41,7 +33,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	// pb.RegisterGreetingServer(grpcServer, &greetingServer{})
+	pb.RegisterActivityServiceServer(grpcServer, &ActivityServer{})
 	reflection.Register(grpcServer)
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
